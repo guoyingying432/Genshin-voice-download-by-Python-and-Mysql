b662698399e71dbaba90acbc1e6e82c74c93db8b
GUO HOUJIAN <62383630+guoyingying432@users.noreply.github.com>
2022-07-02 15:07:38 +0900

    Delete voice_data_AFTER_INSERT.sql

--- voice_data_AFTER_INSERT.sql
+++
@@ -1,3 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `voice_data_AFTER_INSERT` AFTER INSERT ON `voice_data` FOR EACH ROW BEGIN
-UPDATE charcter SET voice_count=voice_count+1 WHERE charcter.char_name=new.char_name;
-END
